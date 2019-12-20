113fa5ad0eacf86c756c79242b557e119fcbacf4
Amaury Séchet <deadalnix@gmail.com>
2019-12-20 17:15:28 +0100

    kill CTxMemPoolEntry::GetPriority
    Summary:
    As per title. It is not useful anymore.
    
    Depends on D4787 and D4788
    
    Test Plan:
      make check
    
    Reviewers: #bitcoin_abc, markblundeberg
    
    Reviewed By: #bitcoin_abc, markblundeberg
    
    Differential Revision: https://reviews.bitcoinabc.org/D4789

--- src/txmempool.cpp
+++ src/txmempool.cpp
@@ -55,18 +55,6 @@ CTxMemPoolEntry::CTxMemPoolEntry(const CTransactionRef &_tx, const Amount _nFee,
     nSigOpCountWithAncestors = sigOpCount;
 }
 
-double CTxMemPoolEntry::GetPriority(unsigned int currentHeight) const {
-    double deltaPriority =
-        double((currentHeight - entryHeight) * (inChainInputValue.toInt())) /
-        nModSize;
-    double dResult = entryPriority + deltaPriority;
-    // This should only happen if it was called with a height below entry height
-    if (dResult < 0) {
-        dResult = 0;
-    }
-    return dResult;
-}
-
 void CTxMemPoolEntry::UpdateFeeDelta(Amount newFeeDelta) {
     nModFeesWithDescendants += newFeeDelta - feeDelta;
     nModFeesWithAncestors += newFeeDelta - feeDelta;
